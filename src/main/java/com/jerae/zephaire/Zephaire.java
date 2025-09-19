0ca5bc1aec64c24e99314a2f30eb86c03f95f5a4
Cabsuu <rikka.takataka@gmail.com>
2025-09-19 14:52:00 +0800

    Follow up

--- src/main/java/com/jerae/zephaire/Zephaire.java
+++ src/main/java/com/jerae/zephaire/Zephaire.java
@@ -37,6 +37,7 @@ public final class Zephaire extends JavaPlugin {
         this.saveDefaultConfig();
         this.saveResource("guide.txt", true);
         this.saveResource("particles.txt", false);
+        this.saveResource("entity-guide.txt", false);
         this.saveResource("disabled-particles.yml", false);
         this.saveResource("entity-particles.yml", false);
 
