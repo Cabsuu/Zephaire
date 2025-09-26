acc8e6ba9f81b2995887bc5da66ccfb9ffde0f23
Cabsuu <rikka.takataka@gmail.com>
2025-09-26 23:23:55 +0800

    Updated to v0.7.0

--- src/main/java/com/jerae/zephaire/particles/factories/conditions/TimerConditionFactory.java
+++ src/main/java/com/jerae/zephaire/particles/factories/conditions/TimerConditionFactory.java
@@ -13,6 +13,6 @@ public class TimerConditionFactory implements ConditionFactory {
     public ParticleCondition create(Map<?, ?> configMap, World defaultWorld, String particlePath, FactoryManager factoryManager) {
         long activeDuration = ConfigValidator.getLong(configMap, "active-duration", 40L, particlePath);
         long cooldownDuration = ConfigValidator.getLong(configMap, "cooldown-duration", 60L, particlePath);
-        return new TimerCondition(activeDuration / 20, cooldownDuration / 20);
+        return new TimerCondition(activeDuration, cooldownDuration);
     }
 }
